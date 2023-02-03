47b941f5ee1bb65108529835f099d5dbacf3ffa7
Adam Measel <ameasel55@gmail.com>
2023-02-03 11:44:36 -0600

    initial commit

---
+++ SelectionStatementsExercise/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace SelectionStatementsExercise;
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        var r = new Random();
+        var favNumber = r.Next(1,10);
+
+        Console.WriteLine("Try to guess my favorite number");
+        var userInput = int.Parse(Console.ReadLine());
+
+        if (userInput < favNumber)
+        {
+            Console.WriteLine("too low");
+        }
+        else if (userInput > favNumber)
+        {
+            Console.WriteLine("too high");
+        }
+        else
+        {
+            Console.WriteLine("Nevermind");
+        }
+
+    }
+}
+
